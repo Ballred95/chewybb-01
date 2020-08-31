f797692080058e6296451be28d4edec10088d59f
BlakeAllred <blake.allred95@icloud.com>
2020-08-30 19:11:54 -0600

    wok

---
+++ main.js
@@ -0,0 +1,17 @@
+const userName = prompt('enter name')
+console.log(userName)
+
+
+document.querySelector('#company').innerHTML='Chewys bed and breakfast'
+document.querySelector('#slogan').innerHTML = 'from a galaxy far far away'
+// hypeDocument.querySelector('.HYPE_element').innerHTML=`Hello ${userName}, I am C3P0 Human Cyborg Relations!`
+// document.querySelector('#text').innerText='hello there'
+
+
+var d = Date(Date.now()); 
+                
+
+
+
+
+
